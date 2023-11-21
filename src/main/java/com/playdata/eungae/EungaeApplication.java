35b7fb3dbd9e8fbd90b8cd6e038c6b0a2aebd832
sooyong <hwasuyong99@naver.com>
2023-11-21 14:14:55 +0900

    first commit

---
+++ src/main/java/com/playdata/eungae/EungaeApplication.java
@@ -0,0 +1,13 @@
+package com.playdata.eungae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EungaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EungaeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/playdata/eungae/EungaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.eungae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EungaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
