f5a82a430e83b932a14cd8edbd38e2fc9137fe73
seanlee0923 <leesinue0923@gmail.com>
2023-12-06 17:06:39 +0900

    [FE] Feat: 진료내역 상세보기 추가
    진료내역 상세보기 기능 추가하였습니다

--- src/main/resources/static/js/medical-records.js
+++ src/main/resources/static/js/medical-records.js
@@ -1,7 +1,16 @@
-const showDetails = () => {
-    console.log("hi");
+const showhDetails = (id) =>{
+    id="detail"+id;
+    if(document.getElementById(id).style.display==='none'){
+        showDetails(id);
+    }else{
+        hideDetails(id);
+    }
 }
 
-const saveDetails = () => {
-    console.log("bye");
+function hideDetails(id)  {
+    document.getElementById(id).style = 'display: none !important';
+  }
+  
+function showDetails(id)  {
+    document.getElementById(id).style = 'display: "" !important';
 }
