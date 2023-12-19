1ad6daa31a6de30a4d91eb3a7278a43c14a7e003
sooyong <hwasuyong99@naver.com>
2023-12-19 16:42:52 +0900

    [BE] Feat: Base Entity 작성

--- src/main/java/com/playdata/eungae/EungaeApplication.java
+++ src/main/java/com/playdata/eungae/EungaeApplication.java
@@ -2,12 +2,14 @@ package com.playdata.eungae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class EungaeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EungaeApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EungaeApplication.class, args);
+	}
 
 }
