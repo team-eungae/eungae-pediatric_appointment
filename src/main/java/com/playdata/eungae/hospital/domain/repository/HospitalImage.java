9f8ebf096380e5db1d794ee0538e617ee96894f1
sooyong <hwasuyong99@naver.com>
2023-12-21 09:22:37 +0900

    [BE] Feat: HospitalImage 엔티티 생성

---
+++ src/main/java/com/playdata/eungae/hospital/domain/repository/HospitalImage.java
@@ -0,0 +1,31 @@
+package com.playdata.eungae.hospital.domain.repository;
+
+import com.playdata.eungae.base.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Table(name = "hospital_image")
+@Entity
+public class HospitalImage extends BaseEntity {
+
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Id
+	private Long hospitalImageSeq;
+
+	// @OneToMany
+	// private Hospital hospitalSeq;
+
+	@Column(nullable = false)
+	private String originFileName;
+
+	@Column(nullable = false)
+	private String storeFileName;
+}
