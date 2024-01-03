1d05c5c319210f8ff4251b31b78b46a651a97398
jun9898 <nice1998@gmail.com>
2024-01-03 09:38:12 +0900

    [BE] Feat : 회원가입 기능 ViewController로 이동

--- src/main/java/com/playdata/eungae/member/controller/MemberViewController.java
+++ src/main/java/com/playdata/eungae/member/controller/MemberViewController.java
@@ -2,11 +2,20 @@ package com.playdata.eungae.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.playdata.eungae.member.dto.SignUpMemberRequestDto;
+import com.playdata.eungae.member.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/my")
+@RequiredArgsConstructor
 public class MemberViewController {
+
+	private final MemberService memberService;
 
 	@GetMapping("/records")
 	public String medicalRecordList() {
@@ -51,5 +60,11 @@ public class MemberViewController {
 	@GetMapping("/profile/form")
 	public String updateProfile() {
 		return "contents/member/my-page-form";
+	}
+
+	@PostMapping("/signup")
+	public String singUp(SignUpMemberRequestDto signUpMemberRequestDto) {
+		memberService.singUp(signUpMemberRequestDto);
+		return "redirect:/login";
 	}
 }
