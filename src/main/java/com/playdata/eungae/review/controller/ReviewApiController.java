8065e562bafe0d4cbce75be9d28db699a5ccc22e
jun9898 <nice1998@gmail.com>
2024-01-04 10:05:01 +0900

    [BE] Feat : 리뷰 페이지 불러오기 작업 완료

--- src/main/java/com/playdata/eungae/review/controller/ReviewApiController.java
+++ src/main/java/com/playdata/eungae/review/controller/ReviewApiController.java
@@ -2,6 +2,7 @@ package com.playdata.eungae.review.controller;
 
 import java.security.Principal;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.playdata.eungae.review.domain.Review;
 import com.playdata.eungae.review.dto.RequestReviewFormDto;
+import com.playdata.eungae.review.dto.ResponseReviewDto;
 import com.playdata.eungae.review.service.ReviewService;
 
 import jakarta.validation.Valid;
@@ -36,15 +38,21 @@ public class ReviewApiController {
 		@Valid @RequestBody RequestReviewFormDto requestReviewFormDto)
 	{
 		reviewService.createReview(appointmentSeq, requestReviewFormDto);
-		// Result 객체를 만들어서 처리하는 방법도 찾아보자
-		return "리뷰 작성 성공";
+		return "success";
 	}
 
 	@PatchMapping("/reviews/{review_seq}")
 	@ResponseStatus(HttpStatus.OK)
 	public String createReview(@PathVariable("review_seq") long reviewSeq) {
 		reviewService.removeReview(reviewSeq);
-		return "리뷰 삭제 성공";
+		return "success";
+	}
+
+	@PostMapping("/reviews/{review_page}")
+	@ResponseStatus(HttpStatus.OK)
+	public Page<ResponseReviewDto> findReviews(@PathVariable("review_page") int reviewPage) {
+		// Result VO 적용시키기
+		return reviewService.findReviews(reviewPage);
 	}
 
 }
