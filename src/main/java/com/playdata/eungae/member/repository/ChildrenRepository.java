6a52cbd30864372102cd22a5e8dd91d281ce2b08
sooyong <hwasuyong99@naver.com>
2024-01-06 16:03:16 +0900

    [BE] Feat: findAllByMemberSeq 및 DoctorRepository 포맷팅

--- src/main/java/com/playdata/eungae/member/repository/ChildrenRepository.java
+++ src/main/java/com/playdata/eungae/member/repository/ChildrenRepository.java
@@ -1,5 +1,8 @@
 package com.playdata.eungae.member.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +10,6 @@ import com.playdata.eungae.member.domain.Children;
 
 @Repository
 public interface ChildrenRepository extends JpaRepository<Children, Long> {
-
-	}
+	Optional<List<Children>> findAllByMemberSeq(long MemberSeq);
+}
 
