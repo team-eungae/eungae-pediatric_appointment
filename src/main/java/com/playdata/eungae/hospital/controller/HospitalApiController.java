fc0a83e3b815339118cceca951cc5592e59e66ce
seanlee0923 <leesinue0923@gmail.com>
2024-01-16 14:21:27 +0900

    [BE] Feat: 병원 키워드검색 유효성 검사
    [BE] Feat: 병원 키워드검색 유효성 검사

--- src/main/java/com/playdata/eungae/hospital/controller/HospitalApiController.java
+++ src/main/java/com/playdata/eungae/hospital/controller/HospitalApiController.java
@@ -5,9 +5,14 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,11 +27,17 @@ import com.playdata.eungae.hospital.service.HospitalImageService;
 import com.playdata.eungae.hospital.dto.HospitalSearchResponseDto;
 import com.playdata.eungae.hospital.service.HospitalService;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/hospital")
+@Validated
 public class HospitalApiController {
 	private final HospitalService hospitalService;
 	private final HospitalImageService hospitalImageService;
@@ -62,7 +73,10 @@ public class HospitalApiController {
 
 	@GetMapping("/search")
 	@ResponseStatus(HttpStatus.OK)
-	public List<HospitalSearchResponseDto> getAllByKeyword(String keyword){
+	public List<HospitalSearchResponseDto> getAllByKeyword(
+		@RequestParam
+		@NotBlank(message = "Keyword can not contain blank")
+		@Size(min = 2, message = "Keyword can not be less than two letters") String keyword) {
 		return hospitalService.findAllByKeyword(keyword);
 	}
 }
