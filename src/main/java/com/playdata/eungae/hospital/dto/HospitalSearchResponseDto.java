fc07d8bd9a1f91537b88af5b2d6d18ad7e51adcb
김우진 <ooojin523@gmail.com>
2024-02-05 14:55:35 +0900

    [BE] Fix: 별점 계산 수정
    별점 계산 수정했습니다.
    This closes #399

--- src/main/java/com/playdata/eungae/hospital/dto/HospitalSearchResponseDto.java
+++ src/main/java/com/playdata/eungae/hospital/dto/HospitalSearchResponseDto.java
@@ -15,45 +15,47 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class HospitalSearchResponseDto {
-	private Long hospitalSeq;
-	private String name;
-	private String hospitalId;
-	private String notice;
-	private int deposit; //예약금
-	private String contact; //연락처
-	private String address;
-	private double longitude;
-	private double latitude;
-	private double averageRating;
-	private int totalReview;
-	private int starCount;
-	private String hospitalThumbnail;
-	private int currentWaitingCount;
+    private Long hospitalSeq;
+    private String name;
+    private String hospitalId;
+    private String notice;
+    private int deposit; //예약금
+    private String contact; //연락처
+    private String address;
+    private double longitude;
+    private double latitude;
+    private String averageRating;
+    private int totalReview;
+    private int starCount;
+    private String hospitalThumbnail;
+    private int currentWaitingCount;
 
-	public static HospitalSearchResponseDto toDto(Hospital entity) {
-		List<Review> reviews = entity.getReviews();
+    public static HospitalSearchResponseDto toDto(Hospital entity) {
+        List<Review> reviews = entity.getReviews();
 
-		double averageRating = reviews.stream()
-			.mapToDouble(Review::getStarRating)
-			.average()
-			.orElse(0.0);
+        double averageRating = reviews.stream()
+                .mapToDouble(Review::getStarRating)
+                .average()
+                .orElse(0.0);
 
-		return HospitalSearchResponseDto.builder()
-			.hospitalSeq(entity.getHospitalSeq())
-			.name(entity.getName())
-			.hospitalId(entity.getHospitalId())
-			.notice(entity.getNotice())
-			.deposit(entity.getDeposit())
-			.contact(entity.getContact())
-			.address(entity.getAddress())
-			.longitude(entity.getXCoordinate())
-			.latitude(entity.getYCoordinate())
-			.averageRating(Math.round(averageRating))
-			.totalReview(reviews.size())
-			.starCount((int)averageRating)
-			.hospitalThumbnail(entity.getHospitalImageList().isEmpty() ?
-				null : entity.getHospitalImageList().get(0).getStoreFileName())
-			.currentWaitingCount(0)
-			.build();
-	}
+        String formattedAverageRating = String.format("%.1f", averageRating);
+
+        return HospitalSearchResponseDto.builder()
+                .hospitalSeq(entity.getHospitalSeq())
+                .name(entity.getName())
+                .hospitalId(entity.getHospitalId())
+                .notice(entity.getNotice())
+                .deposit(entity.getDeposit())
+                .contact(entity.getContact())
+                .address(entity.getAddress())
+                .longitude(entity.getXCoordinate())
+                .latitude(entity.getYCoordinate())
+                .averageRating(formattedAverageRating)
+                .totalReview(reviews.size())
+                .starCount(Integer.parseInt(formattedAverageRating.substring(0, 1)))
+                .hospitalThumbnail(entity.getHospitalImageList().isEmpty() ?
+                        null : entity.getHospitalImageList().get(0).getStoreFileName())
+                .currentWaitingCount(0)
+                .build();
+    }
 }
